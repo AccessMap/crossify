60239edf8293d523c403fc2d968e063f2549bf69
Nick Bolten <nbolten@gmail.com>
2017-11-10 01:41:42 -0800

    Added happy little "Done" messages during execution

--- crossify/__main__.py
+++ crossify/__main__.py
@@ -65,47 +65,65 @@ def core(sidewalks, outfile, debug=False):
     #
 
     # Note: all are converted to WGS84 by default
-    click.echo('Fetching street network from OpenStreetMap...')
+    click.echo('Fetching street network from OpenStreetMap...', nl=False)
+
     G_streets = io.fetch_street_graph(sidewalks)
 
+    click.echo('Done')
+
     # Work in UTM
-    click.echo('Generating street graph...')
+    click.echo('Generating street graph...', nl=False)
+
     G_streets_u = ox.projection.project_graph(G_streets)
     sidewalks_u = ox.projection.project_gdf(sidewalks)
 
     # Get the undirected street graph
     G_undirected_u = ox.save_load.get_undirected(G_streets_u)
 
+    click.echo('Done')
+
     # Extract streets from streets graph
-    click.echo('Extracting geospatial data from street graph...')
+    click.echo('Extracting geospatial data from street graph...', nl=False)
+
     streets = ox.save_load.graph_to_gdfs(G_undirected_u, nodes=False,
                                          edges=True)
     streets.crs = sidewalks_u.crs
+
+    click.echo('Done')
 
     #
     # Isolate intersections that need crossings (degree > 3), group with
     # their streets (all pointing out from the intersection)
     #
-    click.echo('Isolating street intersections...')
+    click.echo('Isolating street intersections...', nl=False)
+
     ixns = intersections.group_intersections(G_streets_u)
+
+    click.echo('Done')
 
     #
     # Draw crossings using the intersection + street + sidewalk info
     #
-    click.echo('Drawing crossings...')
+    click.echo('Drawing crossings...', nl=False)
+
     st_crossings = crossings.make_crossings(ixns, sidewalks_u, debug=debug)
     if debug:
         st_crossings, street_segments = st_crossings
 
+    click.echo('Done')
+
     #
     # Write to file
     #
-    click.echo('Writing to file...')
+    click.echo('Writing to file...', nl=False)
+
     io.write_crossings(st_crossings, outfile)
     if debug:
         base, ext = path.splitext(outfile)
         debug_outfile = '{}_debug{}'.format(base, ext)
         io.write_debug(street_segments, debug_outfile)
+
+    click.echo('Done')
 
 
 if __name__ == '__main__':
